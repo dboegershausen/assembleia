8e7547ee76a6b2b8eb419949a123e5fa67843d54
Diogo Boegershausen <diogo.boegershausen@db1.com.br>
2022-05-20 03:21:29 -0300

    removendo code smells

--- src/main/java/br/com/cooperados/assembleia/core/clients/InformacoesDoUsuarioClient.java
+++ src/main/java/br/com/cooperados/assembleia/core/clients/InformacoesDoUsuarioClient.java
@@ -2,15 +2,13 @@ package br.com.cooperados.assembleia.core.clients;
 
 import br.com.cooperados.assembleia.domain.models.InformacoesDoUsuario;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(value = "userInfo", url = "https://user-info.herokuapp.com/")
 public interface InformacoesDoUsuarioClient {
 
-
-    @RequestMapping(method = RequestMethod.GET, value = "/users/{cpf}", produces = "application/json")
+    @GetMapping(value = "/users/{cpf}", produces = "application/json")
     InformacoesDoUsuario buscar(@PathVariable("cpf") String cpf);
 
 }
--- src/main/java/br/com/cooperados/assembleia/domain/repositories/VotacaoRepository.java
+++ src/main/java/br/com/cooperados/assembleia/domain/repositories/VotacaoRepository.java
@@ -3,10 +3,7 @@ package br.com.cooperados.assembleia.domain.repositories;
 import br.com.cooperados.assembleia.domain.enums.StatusDaVotacao;
 import br.com.cooperados.assembleia.domain.models.Votacao;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
